5fbf66a6526d3984ae2b4ed01b03923001272fa8
mobinmiah <mobinmiah1990@gmail.com>
2025-12-18 09:19:16 +0600

    completed a patch on assets after whole night trying

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ async function run() {
         const packagesCollection = db.collection("packages")
         const usersCollection = db.collection("users")
         const assetsCollection = db.collection("assets")
+        const requestCollection = db.collection('requests')
 
         // verify hr middleware with database access
         const verifyHR = async (req, res, next) => {
@@ -94,6 +95,17 @@ async function run() {
             });
             res.send({ token });
         });
+        // app.post("/jwt", async (req, res) => {
+        //     const { email, name, role } = req.body;
+
+        //     const token = jwt.sign(
+        //         { email, name, role },
+        //         process.env.ACCESS_TOKEN_SECRET,
+        //         { expiresIn: "7d" }
+        //     );
+
+        //     res.send({ token });
+        // });
 
         // global api
         app.get('/', (req, res) => {
@@ -133,15 +145,23 @@ async function run() {
         });
 
         app.get('/users/:email/role', verifyToken, async (req, res) => {
-            const decodecEmail = req.decoded.email
-            const reqEmail = req.params.email
-            if (decodecEmail !== reqEmail) {
+            const email = req.params.email
+            const query = { email }
+            const user = await usersCollection.findOne(query)
+            res.send({ role: user?.role || 'employee' })
+        })
+
+        app.get('/users/:email/role', verifyToken, async (req, res) => {
+            const email = req.params.email
+            const decodedEmail = req.decoded.email
+            if (email !== decodedEmail) {
                 return res.status(403).send({ message: 'Forbidden' });
             }
 
             const user = await usersCollection.findOne({ email: req.params.email });
             res.send({ role: user?.role || 'employee' });
         });
+
 
         // assets apis
         app.post('/assets', verifyToken, verifyHR, async (req, res) => {
@@ -172,12 +192,24 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/assets/public", verifyToken, async (req, res) => {
+            const searchText = req.query.searchText
+            const query = {}
+
+            if (searchText) {
+                query.productName = { $regex: searchText, $options: 'i' }
+            }
+            const cursor = assetsCollection.find(query).sort({ createdAt: -1 })
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
         app.patch('/assets/:id', verifyToken, verifyHR, async (req, res) => {
             const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
             const { productName, productImage, productType, productQuantity } = req.body;
-            const asset = await assetsCollection.findOne({
-                _id: new ObjectId(id)
-            });
+            const asset = await assetsCollection.findOne(query);
 
             if (!asset) {
                 return res.status(404).send({ message: "Asset not found" });
@@ -185,20 +217,7 @@ async function run() {
 
             if (asset.hrEmail !== req.decoded.email) {
                 return res.status(403).send({
-                    message: "You can only edit assets you created"
-                });
-            }
-
-            const assignedQuantity =
-                Number(asset.productQuantity) - Number(asset.availableQuantity);
-            const newproductQuantity =
-                productQuantity !== undefined
-                    ? Number(productQuantity)
-                    : asset.productQuantity;
-
-            if (newproductQuantity < assignedQuantity) {
-                return res.status(400).send({
-                    message: "Total quantity cannot be less than assigned assets"
+                    message: "Forbidden access"
                 });
             }
 
@@ -207,15 +226,11 @@ async function run() {
                     productName: productName || asset.productName,
                     productImage: productImage || asset.productImage,
                     productType: productType || asset.productType,
-                    productQuantity: newproductQuantity,
-                    availableQuantity: newproductQuantity - assignedQuantity,
+                    productQuantity: productQuantity || asset.productQuantity,
                 }
             };
-            const result = await assetsCollection.updateOne(
-                { _id: new ObjectId(id) },
-                updateDoc
-            );
-            res.send({ result });
+            const result = await assetsCollection.updateOne(query, updateDoc);
+            res.send(result);
         });
 
         app.delete('/assets/:id', verifyToken, verifyHR, async (req, res) => {
@@ -232,6 +247,52 @@ async function run() {
             const result = await assetsCollection.deleteOne(query)
             res.send(result)
         })
+
+        // request apis
+        app.post('/asset-requests', verifyToken, async (req, res) => {
+            const {
+                assetId,
+                requestedQuantity,
+                requestNote
+            } = req.body;
+
+            const asset = await assetsCollection.findOne({ _id: new ObjectId(assetId) });
+
+            if (!asset) {
+                return res.status(404).send({ message: "Asset not found" });
+            }
+
+            if (requestedQuantity > asset.availableQuantity) {
+                return res.status(400).send({ message: "Not enough stock available" });
+            }
+
+            const newRequest = {
+                assetId: asset._id,
+                productName: asset.productName,
+                productImage: asset.productImage,
+                productType: asset.productType,
+
+                requesterEmail: req.decoded.email,
+                requesterName: req.decoded.name,
+
+                requestedQuantity,
+                requestNote: requestNote || "",
+
+                companyName: asset.companyName,
+                hrEmail: asset.hrEmail,
+
+                status: "pending",
+                requestedAt: new Date(),
+
+            };
+
+            const result = await requestCollection.insertOne(newRequest);
+            res.send(result);
+        });
+
+
+
+
 
 
         // await client.db("admin").command({ ping: 1 });
